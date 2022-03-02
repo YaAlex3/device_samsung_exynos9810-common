5355f8760cf6704563f30b18213c93c2b86b8909
Alexander <yaalex@yaalex.tk>
2022-03-02 12:14:53 +0000

    exynos9810-common: Define partition structure
    Signed-off-by: Alexander <yaalex@yaalex.tk>

--- Android.mk
+++ Android.mk
@@ -19,8 +19,6 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter r7, $(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
-include $(COMMON_PATH)/AndroidBoard.mk
-
 include $(CLEAR_VARS)
 
 EGL_LIBS := libGLES_mali.so libOpenCL.so libOpenCL.so.1 libOpenCL.so.1.1
--- common.mk
+++ common.mk
@@ -85,6 +85,9 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor:32 \
     android.hardware.drm@1.4-service.clearkey
 
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
