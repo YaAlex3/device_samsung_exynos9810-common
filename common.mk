6ff3140273bacc4b1cefe7b6a88e61a69bb0f157
Alexander <yaalex@yaalex.tk>
2022-03-15 19:31:50 +0000

    exynos9810-common: Blob stock biometrics
    Signed-off-by: Alexander <yaalex@yaalex.tk>

--- common.mk
+++ common.mk
@@ -103,7 +103,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.samsung
+    android.hardware.biometrics.fingerprint@2.1.vendor:64
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
