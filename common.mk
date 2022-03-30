52d32535d6e7654891841f607e6d9f44ff2ac3b5
Alexander <yaalex@yaalex.tk>
2022-03-30 19:24:48 +0200

    exynos9810-common: Use prebuilt libbt-vendor
    * AOSP one missing coex code
    * Uprev bluetooth/audio while we're at it
    
    Signed-off-by: Alexander <yaalex@yaalex.tk>

--- common.mk
+++ common.mk
@@ -24,7 +24,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.audio@7.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.0-impl:32 \
+    android.hardware.bluetooth.audio@2.1-impl:32 \
     android.hardware.soundtrigger@2.0-impl:32 \
     android.hidl.allocator@1.0.vendor \
     audio.a2dp.default \
@@ -49,9 +49,8 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl:64 \
-    android.hardware.bluetooth@1.0-service \
-    libbt-vendor:64
+    android.hardware.bluetooth@1.1-impl:64 \
+    android.hardware.bluetooth@1.1-service \
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/sysconfig/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
